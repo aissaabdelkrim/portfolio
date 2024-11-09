cc88419ab80c871740366d284e0b29e357a5b447
aissa abdelkrim <aissaabdelkrim48@gmail.com>
2024-11-09 18:58:32 +0100

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,4 +13,5 @@ lnk.addEventListener("click", function () {
         lnk.innerHTML ='more>>';
     }
 });
+document.getElementById('contact-form').addEventListener('submit', function(event) { event.preventDefault(); var formData = new FormData(this); fetch(this.action, { method: 'POST', body: formData, headers: { 'Accept': 'application/json' } }).then(response => { if (response.ok) { document.getElementById('contact-form').style.display = 'none'; document.getElementById('thankyou-message').style.display = 'block'; } else { alert('Failed to send message. Please try again.'); } }).catch(error => { console.error('Error:', error); alert('Failed to send message. Please try again.'); }); });
 function closePopup() { document.getElementById('popup').style.display = 'none'; } // Open the popup on page load window.onload = function() { document.getElementById('popup').style.display = 'flex'; }
